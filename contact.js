f0fef88b4bf33b65ddaa7a94d1ac8eea2189f8a7
JWar959 <john.warren959@gmail.com>
2025-03-09 11:19:20 -0400

    View files are correctly being rendered

--- contact.js
+++ contact.js
@@ -1,12 +1,18 @@
 const pug = require('pug');
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended : true})); // enables body parsing
-app.set('view engine', 'pug'); // sets pug as template
 
-app.get('/foo', (req, res) => {
+app.use(express.urlencoded({extended : true})); // enables body parsing
+app.set('view engine', 'pug'); // setting view engine
+
+app.get('/', (req, res) => {
     res.render('foo', {a:10, b:12});
 });
+
+app.post('/', (res, req) => {
+    res.render('bar', {a:10, b:12});
+})
+
 
 app.listen(8080, () => {
     console.log('App listening on port 8080');
